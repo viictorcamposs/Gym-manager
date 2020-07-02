43a8ea0a9f8db606949e117da55a21d9feabcc06
Victor Campos <campos.viictor@gmail.com>
2020-07-02 18:55:02 -0300

    adicionando servidor e rotas

---
+++ server.js
@@ -0,0 +1,21 @@
+const nunjucks = require ('nunjucks')
+const express = require ('express')
+const routes = require ('./routes')
+const server = express ()
+ 
+server.use (express.static ('public/assets'))
+server.use (express.static ('public/styles'))
+server.use (express.static ('public/scripts'))
+server.use (routes)
+
+server.set ('view engine', 'njk')
+
+nunjucks.configure ('views', {
+  express: server,
+  autoescape: false,
+  noCache: true
+})
+
+server.listen (5000, function () {
+  console.log ('Server is Running')
+})
