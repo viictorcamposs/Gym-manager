f87ea7397a01ad01eed663f68953f4459d364b3d
Victor Campos <campos.viictor@gmail.com>
2020-07-03 12:01:11 -0300

    add route cadastro instrutor

--- routes.js
+++ routes.js
@@ -5,4 +5,25 @@ routes.get ('/', function (req, res) {
   return res.render ('home')
 })
 
+// MANIPULANDO PÁGINA INSTRUCTORS 
+  routes.get ('/instructors', function (req, res) {
+    return res.render ('instructors/instructors')
+  })
+  routes.get ('/instructors/create', function (req, res) {
+    return res.render ('instructors/create')
+  })
+  routes.post ('/instructors', function (req, res) {
+    return res.send ('OK')
+  })
+
+
+
+routes.get ('/members', function (req, res) {
+  return res.render ('members/members')
+})
+
+routes.get ('/signin', function (req, res) {
+  return res.render ('signin')
+})
+
 module.exports = routes
