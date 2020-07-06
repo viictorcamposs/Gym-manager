aec9aecdb87b27633e1a50f9082b4862672bf33c
Victor Campos <campos.viictor@gmail.com>
2020-07-06 10:47:05 -0300

    estruturando dados; update create.njk

--- public/scripts/instructors.js
+++ public/scripts/instructors.js
@@ -10,11 +10,22 @@ exports.post = function (req, res) {
   }
 
   // MANIPULANDO DATAS NO BACK-END
-  req.body.birth = Date.parse (req.body.birth) 
-  req.body.created_at = Date.now ()
+  let { avatar_url, birth, name, services, gender } = req.body
+
+  birth = Date.parse (birth) 
+  const created_at = Date.now ()
+  const id = Number (data.instructors.length + 1)
 
   // [] (ENVIANDO DADOS PARA O MEU DATA.JSON (BACK-END) PELO FRONT-END)
-  data.instructors.push (req.body) // [{...}, {...}]
+  data.instructors.push ({
+    id, 
+    avatar_url,
+    name,
+    birth,
+    gender,
+    services,
+    created_at
+  }) // [{...}, {...}]
   fs.writeFile ('data.json', JSON.stringify (data, null, 2), function (error) {
     if (error) {
       return res.send ('Write file error')
